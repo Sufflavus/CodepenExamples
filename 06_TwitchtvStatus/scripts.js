babd78fd7dc137b2070ad11e545fa2afb0b4943f
Tania Shatilova <tatashat@gmail.com>
2016-04-14 00:08:20 +0300

    * Twitch.tv Status

--- 06_TwitchtvStatus/scripts.js
+++ 06_TwitchtvStatus/scripts.js
@@ -1,16 +1,27 @@
 (function() {
   var userNames = ["freecodecamp", "storbeck", "terakilobyte", "habathcx","RobotCaleb","thomasballinger","noobs2ninjas","beohoff", "brunofin", "OgamingSC2"];
+  
   var usersUrl = "https://api.twitch.tv/kraken/users/";
   var streamsUrl = "https://api.twitch.tv/kraken/streams/";
   var dummyImg = "http://dummyimage.com/50x50/bdbdbd/ffffff.png&text=0x3F";
     
   var $users = $("#users");
+  
   var allUserSections = [];
   var onlineUserSections = [];
   var offlineUserSections = [];
+  
+  var tabs = {
+    all: "0",
+    online: "1",
+    offline: "2"
+  };
+  
+  var selectedTab = tabs.all;
     
   initData();
   subscribeTabs();
+  subscribeSearch();
     
   function initData() {
     userNames.forEach(function(name) {
@@ -20,20 +31,18 @@
         .then(function(data) {
           user.name = data.display_name;
           user.logo = data.logo || dummyImg;
-          user.url = "http://twitch.tv/" + data.name;        
-      });
-      
-      $.getJSON(streamsUrl + name)
-        .then(function(data) {          
-          if(data.stream) {            
-            user.message = data.stream.game + ": " + data.stream.channel.status;
-            createOnlineUserSection(user);
-          } else {
-            createOfflineUserSection(user);
-          }            
-        }).fail(function(a, b, c) {
-          createClosedUserSection(user);
-      });      
+          user.url = "http://twitch.tv/" + data.name;                  
+          return $.getJSON(streamsUrl + name)
+        }).then(function(data) {          
+            if(data.stream) {            
+              user.message = data.stream.game + ": " + data.stream.channel.status;
+              showOnlineUserSection(user);
+            } else {
+              showOfflineUserSection(user);
+            }            
+          }).fail(function(a, b, c) {
+            showClosedUserSection(user);
+        });      
     })
   }
   
@@ -41,7 +50,8 @@
     $("#tab_all").click(function() {
       allUserSections.forEach(function(section) {
         section.fadeIn(2000); 
-      });        
+      });    
+      selectedTab = tabs.all;
     });
     
     $("#tab_online").click(function() {
@@ -50,7 +60,8 @@
       }); 
       offlineUserSections.forEach(function(section) {
         section.fadeOut(); 
-      });             
+      });   
+      selectedTab = tabs.online;
     });
     
     $("#tab_offline").click(function() {
@@ -59,13 +70,41 @@
       });  
       offlineUserSections.forEach(function(section) {
         section.fadeIn(); 
-      });              
+      });    
+      selectedTab = tabs.offline;
+    });
+  }
+  
+  function subscribeSearch() {
+    $("#search_field").keyup(function(){
+      var filter = $(this).val().toLowerCase();
+      var arrayForSearch;
+      
+      switch(selectedTab) {
+        case tabs.online:
+          arrayForSearch = onlineUserSections;
+          break;
+        case tabs.offline:
+          arrayForSearch = offlineUserSections;
+          break;
+        default:
+          arrayForSearch = allUserSections;          
+      }
+      
+      arrayForSearch.forEach(function(section) {
+        if(section.data("id").indexOf(filter) >= 0) {
+          section.fadeIn(); 
+        } else {
+          section.fadeOut(); 
+        }        
+      });       
     });
   }
     
-  function createOnlineUserSection(user) {    
+  function showOnlineUserSection(user) {    
     var section = $("<li/>", {        
-      class: "collection-item avatar invisible"
+      class: "collection-item avatar invisible",
+      "data-id": user.name.toLowerCase()
     }).appendTo($users);
 
     var logo = $("<img/>", {
@@ -101,9 +140,10 @@
     onlineUserSections.push(section);
   }
   
-  function createOfflineUserSection(user) {    
+  function showOfflineUserSection(user) {    
     var section = $("<li/>", {        
-      class: "collection-item avatar invisible"
+      class: "collection-item avatar invisible",
+      "data-id": user.name.toLowerCase()
     }).appendTo($users);
 
     var logo = $("<img/>", {
@@ -134,9 +174,10 @@
     offlineUserSections.push(section);
   }
   
-  function createClosedUserSection(user) {        
+  function showClosedUserSection(user) {        
     var section = $("<li/>", {        
-      class: "collection-item avatar invisible"
+      class: "collection-item avatar invisible",
+      "data-id": user.name.toLowerCase()
     }).appendTo($users);
 
     var logo = $("<img/>", {
