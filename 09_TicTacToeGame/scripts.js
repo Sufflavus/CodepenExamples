c2f3898f9361d9e4d1c9e40a7b34ab7b0bba2c79
Tania Shatilova <tatashat@gmail.com>
2016-04-24 22:48:18 +0300

    + Tic Tac Toe Game

---
+++ 09_TicTacToeGame/scripts.js
@@ -0,0 +1,14 @@
+(function() {
+  
+  $("#roleChooser").openModal({
+    dismissible: false, // Modal can be dismissed by clicking outside of the modal
+    opacity: .5, // Opacity of modal background
+    in_duration: 300, // Transition in duration
+    out_duration: 200, // Transition out duration      
+  });
+  //http://www3.ntu.edu.sg/home/ehchua/programming/java/javagame_tictactoe_ai.html
+  //https://github.com/aglemann/tic-tac-toe
+  function Game() {
+    
+  } 
+})();
