87e995ed76b290bc442a1cfb187097184c953261
Tania Shatilova <tatashat@gmail.com>
2016-04-29 09:13:32 +0300

    * Tic Tac Toe Game finished

--- 09_TicTacToeGame/scripts.js
+++ 09_TicTacToeGame/scripts.js
@@ -1,7 +1,4 @@
-(function() {
-        //http://www3.ntu.edu.sg/home/ehchua/programming/java/javagame_tictactoe_ai.html
-  var userRole = "X";
-  
+(function() {        
   var markerType = {
     x: "X",
     o: "O",
@@ -9,7 +6,8 @@
   };
   
   var game = new Game();
-  
+  drawBoard(game.board);
+        
   $("#roleChooser").openModal({
     dismissible: false, // Modal can be dismissed by clicking outside of the modal
     opacity: .5, // Opacity of modal background
@@ -17,141 +15,133 @@
     out_duration: 300,    
   });
   
-  $("#btnX").click(function() {
-    userRole = "X";    
-    clearField();
-    game.setStrangerMarker(markerType.x);    
+  $("#btnX").click(function() {    
+    game.initPlayerMarkers(markerType.x);    
   });
   
-  $("#btnO").click(function() {
-    userRole = "O";    
-    clearField();
-    game.setStrangerMarker(markerType.o);    
+  $("#btnO").click(function() {    
+    game.initPlayerMarkers(markerType.o);    
   });
   
   $(".s4").click(function() {
-    var element = $(this);
-    if(!element.text()) {
-      //element.text(userRole);       
+    var element = $(this).children().first();
+    if(!element.text()) {             
       var position = element.data("position").split("-");
       game.onStrangerMove([+position[0], +position[1]]);
     }
-  });
+  });   
   
-  function clearField() {
-    $(".s4").each(function() {
-      $(this).text("");
-    });
-  } 
+  function drawBoard(board) {
+    var $board = $("#board");
+
+    for (var row = 0; row < board.rowsCount; row++) {
+      var $row = $("<div/>", {        
+        class: "row" + (row == board.rowsCount - 1 ? "" : " bordered")
+      }).appendTo($board);
+
+      for (var col = 0; col < board.colsCount; col++) {
+        var $cell = $("<div/>", {        
+          class: "col s4 center-align waves-effect waves-green" + (col == board.colsCount - 1 ? "" : " bordered")         
+        }).appendTo($row);
+        var content = $("<span/>", {                 
+          attr: {
+            "data-position": row + "-" + col
+          }
+        }).text((row + col) % 2 == 0 ? markerType.x : markerType.o).appendTo($cell);
+      }
+    }
+  }
   
-  //http://www3.ntu.edu.sg/home/ehchua/programming/java/javagame_tictactoe_ai.html
-  //https://github.com/aglemann/tic-tac-toe
   function Game() {
     var self = this;
     var board = new Board();
     var stranger = new Stranger();
-    var ai = new Ai(board);
-    var firstPlayer = markerType.x;
+    var ai = new Ai(board);    
     var winnerHighlightClass = "green-text text-accent-3";
     
-    var winningPatterns = [
-      //0x111000000, 0x000111000, 0x000000111, // rows
-      448, 56, 7,
-      //0x100100100, 0x010010010, 0x001001001, // cols
-      292, 146, 73,
-      //0x100010001, 0x001010100               // diagonals
-      273, 84
-    ];
+    self.board = board;
     
-    var winCombinationByPattern = {
-      448: [[0, 0], [0, 1], [0, 2]],
-      56: [[1, 0], [1, 1], [1, 2]],
-      7: [[2, 0], [2, 1], [2, 2]],
-      292: [[0, 0], [1, 0], [2, 0]],
-      146: [[0, 1], [1, 1], [2, 1]],
-      73: [[0, 2], [1, 2], [2, 2]],
-      273: [[0, 0], [1, 1], [2, 2]],
-      84: [[2, 0], [1, 1], [0, 2]]
-    }
-    
-    self.setStrangerMarker = function(marker) {
-      stranger.setMarker(marker);
-      var aiMarker = marker == markerType.o ? markerType.x : markerType.o;
-      ai.setMarker(aiMarker);
+    self.initPlayerMarkers = function(strangerMarker) {
+      stranger.initMarker(strangerMarker);
+      stranger.isFirstPlayer = strangerMarker == markerType.x;
       
-      if(aiMarker === firstPlayer) {
-        var aiMove = ai.getNextMove();
-        board.putMarker(aiMove, ai.myMarker);
-        drawMarker(aiMove, ai.myMarker);
-      }
+      var aiMarker = strangerMarker == markerType.o ? markerType.x : markerType.o;
+      ai.initMarker(aiMarker); 
+      ai.isFirstPlayer = !stranger.isFirstPlayer;
+      
+      restart(); 
     }
     
     self.onStrangerMove = function(position) {
-      if (board.getMarker(position) == markerType.none) {
-        board.putMarker(position, stranger.myMarker);
+      if (board.isFieldEmpty(position)) {
+        board.setMarker(position, stranger.myMarker);
         drawMarker(position, stranger.myMarker);
         
-        var isWin = checkIfWin(stranger.myMarker);
-        if(isWin.hasWon) {
-          alert(stranger.myMarker);
+        var isWin = board.checkIfWin(stranger.myMarker);
+        if(isWin.hasWon) {          
           highlightWin(isWin.winCombination);
+          setTimeout(restart, 2000);          
+          return;
+        }
+        
+        if(!board.hasEmptyFields()) {
+          setTimeout(restart, 2000);    
           return;
         }
         
         var aiMove = ai.getNextMove();
-        board.putMarker(aiMove, ai.myMarker);
+        board.setMarker(aiMove, ai.myMarker);
         drawMarker(aiMove, ai.myMarker);
         
-        isWin = checkIfWin(ai.myMarker);
-        if(isWin.hasWon) {
-          alert(ai.myMarker);
+        isWin = board.checkIfWin(ai.myMarker);
+        if(isWin.hasWon) {          
           highlightWin(isWin.winCombination);
+          setTimeout(restart, 2000);    
           return;
         }
+        
+        if(!board.hasEmptyFields()) {
+          setTimeout(restart, 2000);
+          return;
+        }
+      }
+    }       
+    
+    function restart() {
+      clearField();
+      if(ai.isFirstPlayer) {
+        var aiMove = ai.getNextMove();
+        board.setMarker(aiMove, ai.myMarker);
+        drawMarker(aiMove, ai.myMarker);
       }
     }
            
     function drawMarker(position, marker) {
-      $('.s4[data-position="' + position[0] + '-' + position[1] + '"]').text(marker);
+      $('.s4 span[data-position="' + position[0] + '-' + position[1] + '"]').hide().text(marker).fadeIn(1000);
     }
     
-    function highlightWin(winCombination) {
+    function clearField() {
+      board.clear();
+      $(".s4 span").each(function() {
+        $(this).fadeOut(1000).removeClass(winnerHighlightClass).text("").show();
+      });
+    } 
+    
+    function highlightWin(winCombination) {      
       for(var i = 0; i < winCombination.length; i++) {
         var coors = winCombination[i].join("-");
-        $('.s4[data-position="' + coors + '"]').addClass(winnerHighlightClass).fadeOut(500).fadeIn(500);
+        $('.s4 span[data-position="' + coors + '"]').addClass(winnerHighlightClass);
       }
-    }
-    
-    /** Returns true if thePlayer wins */
-    function checkIfWin(playerMarker) {
-      var pattern = 0;  // 9-bit pattern for the 9 cells
-      for (var row = 0; row < board.rowsCount; row++) {
-        for (var col = 0; col < board.colsCount; col++) {
-          if (board.getMarker([row, col]) == playerMarker) {
-            pattern |= (1 << (row * board.colsCount + col));
-          }
-        }
-      }
-      
-      for (var i = 0, length = winningPatterns.length; i < length; i++) {
-        var winningPattern = winningPatterns[i];
-        if ((pattern & winningPattern) == winningPattern) {
-          return {
-            hasWon: true,
-            winCombination: winCombinationByPattern[winningPattern]
-          };
-        }
-      }
-      return { hasWon: false };
-    }; 
+    }      
   } 
   
   function Player() {
     var self = this;
     self.myMarker = markerType.none; 
     self.opponentMarker = markerType.none;   
+    self.isFirstPlayer = false;
                   
-    self.setMarker = function(marker) {
+    self.initMarker = function(marker) {
       var self = this;
       var opponentMarker = marker == markerType.o ? markerType.x : markerType.o;
       self.myMarker = marker;      
@@ -172,6 +162,7 @@
     self.board = board;    
 
     self.getNextMove = function() {
+      // the algorithm is based on article http://www3.ntu.edu.sg/home/ehchua/programming/java/javagame_tictactoe_ai.html
       var result = minimax(2, self.myMarker, -Number.MAX_VALUE, Number.MAX_VALUE);
       return [result[1], result[2]];   // row, col
     }
@@ -180,7 +171,7 @@
       // Generate possible next moves in a list of int[2] of {row, col}.
       var nextMoves = generateMoves();
  
-      // mySeed is maximizing; while oppSeed is minimizing
+      // myMarker is maximizing; while opponentMarker is minimizing
       var score;
       var bestRow = -1;
       var bestCol = -1;
@@ -193,15 +184,15 @@
          for (var i = 0, length = nextMoves.length; i < length; i++) {
           var move = nextMoves[i];
             // try this move for the current "player"
-            self.board.putMarker(move, marker);
-            if (marker == self.myMarker) {  // mySeed (computer) is maximizing player
+            self.board.setMarker(move, marker);
+            if (marker == self.myMarker) {  // myMarker (computer) is maximizing player
                score = minimax(depth - 1, self.opponentMarker, alpha, beta)[0];
                if (score > alpha) {
                   alpha = score;
                   bestRow = move[0];
                   bestCol = move[1];
                }
-            } else {  // oppSeed is minimizing player
+            } else {  // opponentMarker is minimizing player
                score = minimax(depth - 1, self.myMarker, alpha, beta)[0];
                if (score < beta) {
                   beta = score;
@@ -210,7 +201,7 @@
                }
             }
             // undo move
-            self.board.putMarker(move, markerType.none);
+            self.board.setMarker(move, markerType.none);
             // cut-off
             if (alpha >= beta) {
               break;
@@ -225,15 +216,10 @@
     function generateMoves() {
       var nextMoves = [];
 
-      // If gameover, i.e., no next move
-      /* if (self.hasWon(self.myMarker) || self.hasWon(self.opponentMarker)) {
-        return nextMoves;   // return empty list
-      } */
-
       // Search for empty cells and add to the List
       for (var row = 0; row < board.rowsCount; row++) {
         for (var col = 0; col < board.colsCount; col++) {
-          if (self.board.getMarker([row, col]) == markerType.none) {
+          if (self.board.isFieldEmpty([row, col])) {
             nextMoves.push([row, col]);
           }
         }
@@ -275,17 +261,17 @@
 
       // Second cell
       if (self.board.getMarker([row2, col2]) == self.myMarker) {
-        if (score == 1) {   // cell1 is mySeed
+        if (score == 1) {   // cell1 is myMarker
           score = 10;
-        } else if (score == -1) {  // cell1 is oppSeed
+        } else if (score == -1) {  // cell1 is opponentMarker
           return 0;
         } else {  // cell1 is empty
           score = 1;
         }
       } else if (self.board.getMarker([row2, col2]) == self.opponentMarker) {
-        if (score == -1) { // cell1 is oppSeed
+        if (score == -1) { // cell1 is opponentMarker
           score = -10;
-        } else if (score == 1) { // cell1 is mySeed
+        } else if (score == 1) { // cell1 is myMarker
           return 0;
         } else {  // cell1 is empty
           score = -1;
@@ -294,17 +280,17 @@
 
       // Third cell
       if (self.board.getMarker([row3, col3]) == self.myMarker) {
-        if (score > 0) {  // cell1 and/or cell2 is mySeed
+        if (score > 0) {  // cell1 and/or cell2 is myMarker
           score *= 10;
-        } else if (score < 0) {  // cell1 and/or cell2 is oppSeed
+        } else if (score < 0) {  // cell1 and/or cell2 is opponentMarker
           return 0;
         } else {  // cell1 and cell2 are empty
           score = 1;
         }
       } else if (self.board.getMarker([row3, col3]) == self.opponentMarker) {
-        if (score < 0) {  // cell1 and/or cell2 is oppSeed
+        if (score < 0) {  // cell1 and/or cell2 is opponentMarker
           score *= 10;
-        } else if (score > 1) {  // cell1 and/or cell2 is mySeed
+        } else if (score > 1) {  // cell1 and/or cell2 is myMarker
           return 0;
         } else {  // cell1 and cell2 are empty
           score = -1;
@@ -318,7 +304,18 @@
     var rowsCount = 3;
     var colsCount = 3;
     var cells = new Array(rowsCount);
-       
+        
+    var winCombinations = [
+      [[0, 0], [0, 1], [0, 2]],  // row 0
+      [[1, 0], [1, 1], [1, 2]],  // row 1
+      [[2, 0], [2, 1], [2, 2]],  // row 2
+      [[0, 0], [1, 0], [2, 0]],  // col 0
+      [[0, 1], [1, 1], [2, 1]],  // col 1
+      [[0, 2], [1, 2], [2, 2]],  // col 2
+      [[0, 0], [1, 1], [2, 2]],  // diagonal
+      [[2, 0], [1, 1], [0, 2]]   // alternate diagonal
+    ];    
+    
     for(var row = 0; row < rowsCount; row++) {
       cells[row] = new Array(colsCount);
       for(var col = 0; col < colsCount; col++) {
@@ -326,19 +323,63 @@
       }
     }
     
-    function putMarker(position, marker) {
+    function isFieldEmpty(position) {
+      return cells[position[0]][position[1]] == markerType.none;
+    }
+    
+    function setMarker(position, marker) {
       cells[position[0]][position[1]] = marker;  
     }
     
     function getMarker(position) {
       return cells[position[0]][position[1]];
-    }    
+    }   
+    
+    /** Returns true if thePlayer wins */
+    function checkIfWin(playerMarker) { 
+      for(var i = 0, length = winCombinations.length; i < length; i++) {
+        var combination = winCombinations[i];
+        var hasWon = combination.every(function(coors) {
+          return getMarker(coors) == playerMarker;
+        });
+        if(hasWon) {
+          return {
+            hasWon: true,
+            winCombination: combination
+          };
+        }
+      }  
+      return { hasWon: false };      
+    }; 
+    
+    function hasEmptyFields() {
+      for (var row = 0; row < rowsCount; row++) {
+        for (var col = 0; col < colsCount; col++) {
+          if (isFieldEmpty([row, col])) {
+            return true;
+          }
+        }
+      }
+      return false;
+    }
+    
+    function clear() {
+      for(var row = 0; row < rowsCount; row++) {        
+        for(var col = 0; col < colsCount; col++) {
+          cells[row][col] = markerType.none;
+        }
+      }
+    }
         
     return {
       rowsCount: rowsCount,
       colsCount: colsCount,
-      putMarker: putMarker,
-      getMarker: getMarker
+      isFieldEmpty: isFieldEmpty,
+      setMarker: setMarker,
+      getMarker: getMarker,
+      checkIfWin: checkIfWin,
+      hasEmptyFields: hasEmptyFields,
+      clear: clear
     };
   }
 })();
--- README.md
+++ README.md
@@ -57,3 +57,10 @@ Source code for codepen experiments that I have done
 - [Materialize](https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/css/materialize.min.css) 
 - [jQuery 2.2.2](//cdnjs.cloudflare.com/ajax/libs/jquery/2.2.2/jquery.min.js) 
 - [moment.js 2.13.0](http://momentjs.com/downloads/moment.min.js) 
+
+## 9. Tic Tac Toe Game
+
+[Tic Tac Toe Game](http://codepen.io/sufflavus/full/RaBqje/) is based on 
+- [Materialize](https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/css/materialize.min.css) 
+- [jQuery 2.2.2](//cdnjs.cloudflare.com/ajax/libs/jquery/2.2.2/jquery.min.js) 
+- [Case Study on Tic-Tac-Toe Part 2: With AI](http://www3.ntu.edu.sg/home/ehchua/programming/java/javagame_tictactoe_ai.html) 
