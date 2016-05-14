9efb607706ae10f9872f598b58baebc2d096bc72
Tania Shatilova <tatashat@gmail.com>
2016-05-14 22:23:21 +0300

    + title

--- 11_BarChart/scripts.js
+++ 11_BarChart/scripts.js
@@ -1,32 +1,47 @@
 //http://chimera.labs.oreilly.com/books/1230000000345/index.html
+//https://bl.ocks.org/mbostock/7441121
 
 (function(){
-  var w = 1000;
-  var h = 600;
+  var canvasWidth = 1050;
+  var canvasHeight = 650;
+  
+  var chartWidth = 1000;
+  var chartHeight = 680;
+  
+  var dataUrl = "https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json";
   
   var svg = d3.select("div")
     .append("svg")
     .attr({
-      width: w,
-      height: h
-    });
+      width: canvasWidth,
+      height: canvasHeight
+    })
+    .append("g");
   
-  d3.json("https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json", function(error, data) {
+  d3.json(dataUrl, function(error, data) {
     if(error) {
-      console.log(error);
+      //console.log(error);
       return;
     } else {
-      console.log(data);
-      var dataSet = data.data;
-      var minValue = 0;
-      var maxValue = dataSet[0][1];
+      //console.log(data);
+      var name = data.name;
+      var dataSet = data.data; 
+      
       var minYear = new Date(data.from_date).getYear() + 1900;
       var maxYear = new Date(data.to_date).getYear() + 1900;
       
-      dataSet.forEach(function(item) {
-        var value = item[1];        
-        maxValue = value > maxValue  ? value : maxValue;
-      });
+      var minValue = 0;   
+      var maxValue = d3.max(dataSet, function(item) {
+        return item[1];
+      }); 
+      
+      var title = svg.append("text")        
+        .attr({
+          class: "title",
+          x: canvasWidth/2,
+          y: 50
+        })
+        .text(name);
       
       /* console.log(minValue)
       console.log(maxValue)
@@ -47,8 +62,15 @@
       var yAxis = d3.svg.axis()
         .scale(y)
         .orient("left");
+      
+      var chart = svg.append("g")        
+        .attr({
+          width: chartWidth,
+          height: chartHeight,
+          transform: "translate(0, 70)"
+        });
     }
 
-    //buildLine();
+    //buildLine(); #6FB88F
   });
 })();
