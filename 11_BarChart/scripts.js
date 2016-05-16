260f7ec423a4aad98989221921f7841c2f6c89c3
Tania Shatilova <tatashat@gmail.com>
2016-05-16 23:31:37 +0300

    + scales + bars

--- 11_BarChart/scripts.js
+++ 11_BarChart/scripts.js
@@ -5,8 +5,8 @@
   var canvasWidth = 1050;
   var canvasHeight = 650;
   
-  var chartWidth = 1000;
-  var chartHeight = 550;
+  var chartWidth = 900;
+  var chartHeight = 470;
   
   var dataUrl = "https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json";
   
@@ -26,16 +26,24 @@
       //console.log(data);
       var name = data.name;
       var description = data.description;
-      var dataSet = data.data; 
       
-      var minYear = new Date(data.from_date).getYear() + 1900;
-      var maxYear = new Date(data.to_date).getYear() + 1900;
+      var dataSet = data.data.map(function(item) {
+        return {
+          date: new Date(item[0]),
+          value: item[1]
+        };
+      }); 
+      
+      var dataSetLength = dataSet.length;
+      
+      var minDate = new Date(data.from_date);
+      var maxDate = new Date(data.to_date);
       
       var minValue = 0;   
       var maxValue = d3.max(dataSet, function(item) {
-        return item[1];
+        return item.value;
       }); 
-      
+            
       var title = svg.append("text")        
         .attr({
           class: "title",
@@ -50,11 +58,13 @@
       console.log(minYear)
       console.log(maxYear) */
       
-      var x = d3.scale.linear()
-        .range([maxValue, minValue]);
+      var x = d3.time.scale()
+	      .domain([minDate, maxDate])
+        .range([0, chartWidth]);
 
       var y = d3.scale.linear()
-        .range([maxYear, minYear]);
+        .domain([minValue, maxValue])
+        .range([chartHeight, 0]);
 
       var xAxis = d3.svg.axis()
         .scale(x)
@@ -68,14 +78,39 @@
         .attr({
           width: chartWidth,
           height: chartHeight,
-          transform: "translate(0, 70)"
+          transform: "translate(80, 70)"
+        });
+      
+      chart.append("g")
+        .attr({
+          class: "axis",
+          transform: "translate(0," + chartHeight + ")"        
+        }) 
+        .call(xAxis);
+
+      chart.append("g")
+        .attr({
+          class: "axis"                 
+        })
+        .call(yAxis);
+      
+      chart.selectAll(".bar")
+        .data(dataSet) 
+        .enter()
+        .append("rect")
+        .attr({
+          class: "bar",
+          x: function(d) { return x(d.date); },
+          y: function(d) { return y(d.value); },
+          height: function(d) { return chartHeight - y(d.value); },
+          width: chartWidth / dataSetLength
         });
       
       var descriptionText = svg.append("text")        
         .attr({
           class: "description",          
           x: canvasWidth/2,
-          y: canvasHeight - 50,
+          y: canvasHeight - 60,
           dy: 20
         })
         //.call(wrap, canvasWidth);
@@ -89,8 +124,7 @@
   
   //code is from here https://github.com/d3/d3/issues/1642
   function wrap(text, width) {
-    text.each(function() {
-      debugger
+    text.each(function() {      
       var text = d3.select(this),
           words = text.text().split(/\s+/).reverse(),
           word,
