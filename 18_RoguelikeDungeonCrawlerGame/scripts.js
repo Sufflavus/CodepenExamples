a6db8d113d4ca614b5da94f94d6d50f3db6bb101
Tania Shatilova <tatashat@gmail.com>
2016-05-28 23:10:58 +0300

    * DungeonGenerator

--- 18_RoguelikeDungeonCrawlerGame/scripts.js
+++ 18_RoguelikeDungeonCrawlerGame/scripts.js
@@ -2,12 +2,18 @@
 //http://gamedevelopment.tutsplus.com/tutorials/create-a-procedurally-generated-dungeon-cave-system--gamedev-10099
 //http://bigbadwofl.me/random-dungeon-generator/
 
+var cellType = {
+  wall: 0,
+  room: 1,
+  corridor: 2
+};
+
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 class DungeonGenerator {
-  constuctor() {
+  constructor() {
     this.size = {
       n: 100,
       m: 100
@@ -23,6 +29,7 @@ class DungeonGenerator {
   
   generate() {
     this.clearMap();
+    this.generateRooms();
     this.addRooms();
     this.addCorridors();
   }
@@ -31,14 +38,14 @@ class DungeonGenerator {
     for (let i = 0; i < this.size.n; i++) {
       this.map[i] = [];
       for (let j = 0; j < this.size.m; j++) {
-        this.map[i][j] = 0;
+        this.map[i][j] = cellType.wall;
       }
     }
   }
   
-  addRooms() {
+  generateRooms() {
     for (let i = 0; i < this.roomsCount; i++) {
-      let room = creareRandomRoom();
+      let room = this.createRandomRoom();
 
       if (this.isRoomsIntersect(room)) {
         i--;
@@ -49,28 +56,97 @@ class DungeonGenerator {
     }
   }  
   
-  addCorridors() {}
+  addRooms() {
+    var self = this;
+    self.rooms.forEach(function(room) {
+      for (let i = room.x0; i < room.x1; i++) {
+        for (let j = room.y0; j < room.y1; j++) {
+          self.map[i][j] = cellType.room;
+        }
+      }
+    });    
+  }  
   
-  creareRandomRoom() {
-    return {
-      x: getRandomInt(1, this.size.n - this.maxRoomSize),
-      y: getRandomInt(1, this.size.m - this.maxRoomSize),
-      width: getRandomInt(this.minRoomSize, this.maxRoomSize),
-      height: getRandomInt(this.minRoomSize, this.maxRoomSize)
+  addCorridors() {
+    for (let i = 0; i < this.roomsCount; i++) {
+      var roomA = this.rooms[i];
+      var roomB = this.findClosestRoom(roomA);
+
+      var pointA = {
+        x: getRandomInt(roomA.x0, roomA.x1),
+        y: getRandomInt(roomA.y0, roomA.y1)
+      };
+      
+      var pointB = {
+        x: getRandomInt(roomB.x0, roomB.x1),
+        y: getRandomInt(roomB.y0, roomB.y1)
+      };
+
+      while ((pointB.x != pointA.x) || (pointB.y != pointA.y)) {
+        if (pointB.x != pointA.x) {
+          if (pointB.x > pointA.x) {
+            pointB.x--;
+          } else {
+            pointB.x++;
+          }
+        } else if (pointB.y != pointA.y) {
+          if (pointB.y > pointA.y) {
+            pointB.y--;
+          }
+          else {
+            pointB.y++;
+          }
+        }
+
+        this.map[pointB.x][pointB.y] = cellType.corridor;
+      }
+    }      
+  }
+  
+  findClosestRoom(room) {
+    var middle = {
+      x: room.x0 + ((room.x1 - room.x0) / 2),
+      y: room.y0 + ((room.y1 - room.y0) / 2)
+    };
+    var closest = null;
+    var closestDistance = 1000;
+    for (var i = 0; i < this.roomsCount; i++) {
+      var check = this.rooms[i];
+      if (check == room) {
+        continue;
+      }
+      var checkMiddle = {
+        x: check.x0 + ((check.x1 - check.x0) / 2),
+        y: check.y0 + ((check.y1 - check.y0) / 2)
+      };
+      var distance = Math.min(Math.abs(middle.x - checkMiddle.x) - ((room.x1 - room.x0) / 2) - ((check.x1 - check.x0) / 2), Math.abs(middle.y - checkMiddle.y) - ((room.y1 - room.y0) / 2) - ((check.y1 - check.y0) / 2));
+      if (distance < closestDistance) {
+        closestDistance = distance;
+        closest = check;
+      }
+    }
+    return closest;
+  }
+  
+  createRandomRoom() {
+    let x0 = getRandomInt(1, this.size.n - this.maxRoomSize);
+    let y0 = getRandomInt(1, this.size.n - this.maxRoomSize);    
+    let width = getRandomInt(this.minRoomSize, this.maxRoomSize);
+    let height = getRandomInt(this.minRoomSize, this.maxRoomSize);
+    
+    return {      
+      x0: x0,
+      y0: y0,
+      x1: x0 + width,
+      y1: y0 + height
     };
   }
   
-  isRoomsIntersect(room, ignoreIndex) {
-    for (let i = 0; i < this.rooms.length; i++) {
-      if (i == ignoreIndex) {
-        continue;
-      }
-      
+  isRoomsIntersect(room) {
+    for (let i = 0; i < this.rooms.length; i++) {          
       var roomForCheck = this.rooms[i];
-      if (!((room.x + room.width < roomForCheck.x) || 
-            (room.x > roomForCheck.x + roomForCheck.width) || 
-            (room.y + room.height < roomForCheck.y) || 
-            (room.y > roomForCheck.y + roomForCheck.height))) {
+      if (!((room.x1 < roomForCheck.x0) || (room.x0 > roomForCheck.x1) || 
+            (room.y1 < roomForCheck.y0) || (room.y0 > roomForCheck.y1))) {
         return true;
       } 
     }
