8973461c29c51471b50bc8a92680569b2195f452
Tania Shatilova <tatashat@gmail.com>
2016-10-26 01:22:32 +0400

    - protocols from links

--- 03_RandomQuoteMachine/scripts.js
+++ 03_RandomQuoteMachine/scripts.js
@@ -3,30 +3,48 @@
   var $quoteContent = $("blockquote p");
   var $quoteTitle = $("blockquote footer");
   var $btnTweet = $("#btnTweet");
+  var $btnRefresh = $("#btnRefresh");
   
   showNewQuote();
   
-  $("#btnRefresh").click(showNewQuote);
+  $btnRefresh.click(showNewQuote);
   
   function showNewQuote() {
-    getQuote(updateQuote);
+    getQuote().then(updateQuote);
   }
   
-  function getQuote(callBack) {
-    var ts = new Date().getTime();
-    $.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=" + "&_=" + ts, function(result) {
-      var quoteData = result[0];
-      var content = $(quoteData.content).text();
-      callBack(content, quoteData.title);
-    });
-  }  
+  function getQuote() {
+    var url = getNewQuoteUrl();
+    return $.getJSON(url)
+      .then(function(result) {      
+        var quoteData = result[0];
+        var content = $(quoteData.content).text();
+      
+        return {
+          content: content,
+          title: quoteData.title
+        };        
+      });
+  }    
   
-  function updateQuote(content, title) {
+  function updateQuote(quote) {
     $quote.fadeOut(500, function() {
-        $quoteContent.html(content);
-        $quoteTitle.text(title);
-        $(this).fadeIn(500);        
-        $btnTweet.attr('href', 'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text='+ encodeURIComponent('"' + content + '" ' + title));
+        $quoteContent.html(quote.content);
+        $quoteTitle.text(quote.title);
+        $(this).fadeIn(500);      
+        var tweetUrl = getTweetUrl(quote);
+        $btnTweet.attr("href", tweetUrl);
     });   
+  }
+  
+  function getNewQuoteUrl() {
+    var ts = new Date().getTime();
+    return "//quotesondesign.com/wp-json/posts?" + 
+      "filter[orderby]=rand&filter[posts_per_page]=1&callback=&_=" + ts;
+  }
+  
+  function getTweetUrl(quote) {
+    return "//twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=" + 
+      encodeURIComponent("'" + quote.content + "' " + quote.title);
   }
 })();
