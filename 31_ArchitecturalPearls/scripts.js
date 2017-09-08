f5b747a8a246b89d11def2ea0f1b0bde745a64ab
Sufflavus <tatashat@gmail.com>
2017-09-08 19:26:39 +0300

    + loading points from geojson

--- 31_ArchitecturalPearls/scripts.js
+++ 31_ArchitecturalPearls/scripts.js
@@ -1,25 +1,80 @@
-(function initMap() {
-  var geocoder = new google.maps.Geocoder();
-  var uluru = {lat: 60.2049773, lng: 24.9612825};
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 4,
-    center: uluru
-  });
-  geocoder.geocode({ 'address': "Suvorovskiy prosp., 4, Vyborg, Leningradskaya oblast', Russia, 188800"}, geocodeCallBack);
+(function initMap(google) {    
+  let infoWindow = new google.maps.InfoWindow();
+  let map;
   
-  function geocodeCallBack(results, status) {
-    if (status == 'OK') {
-      map.setCenter(results[0].geometry.location);
-      var marker = new google.maps.Marker({
-        map: map,
-        position: results[0].geometry.location
-      });
-    } else {
-      alert('Geocode was not successful for the following reason: ' + status);
-    }
+  init();
+  
+  function init() {
+    let centerPoint = {lat: 60.2049773, lng: 24.9612825};
+    
+    map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 4,
+      center: centerPoint
+    });
+
+    map.data.loadGeoJson("//raw.githubusercontent.com/Sufflavus/CodepenExamples/master/buildingsGeodata.json");    
+    infoWindow.setOptions({pixelOffset: new google.maps.Size(0, -30)});
+    map.data.addListener('click', onMarkerClick);
   }
-  /*var marker = new google.maps.Marker({
-    position: uluru,
-    map: map
-  });*/
-})();
+  
+  function onMarkerClick(event) {
+    const position = event.feature.getGeometry().get();
+    let content = buildInfoWindowContent(event.feature);
+
+    infoWindow.setContent(content);
+    infoWindow.setPosition(position);
+    infoWindow.open(map);
+  }
+  
+  function buildInfoWindowContent(pointData) {
+    const name = pointData.getProperty('name');
+    const address = pointData.getProperty('address');    
+    const architect = pointData.getProperty('architect');
+    const year = pointData.getProperty('year');
+    const imageUrl = pointData.getProperty('imageUrl');
+    const imageCopyright = pointData.getProperty('imageCopyright');
+    const link = pointData.getProperty('link');
+    let content = sanitizeHTML`
+      <div class="info-window">
+        <div class="info-window__header">${name}</div> 
+        <div class="info-window__image-wrapper">
+          <img class="info-window__image" src="${imageUrl}"/>
+          <div class="info-window__image_info">${imageCopyright}</div>   
+        </div>        
+        <div class="info-window__content">
+          <div class="info-window__row">
+            <span class="info-window__label">Address:</span>&nbsp;${address}
+          </div>
+          <div class="info-window__row">
+            <span class="info-window__label">Architect:</span>&nbsp;${architect}
+          </div>
+          <div class="info-window__row">
+            <span class="info-window__label">Year:</span>&nbsp;${year}
+          </div>
+          <div class="info-window__row">
+            <span class="info-window__label">More info:</span>&nbsp;
+            <a href="${link}">link</a>
+          </div> 
+        </div>
+      </div>
+    `;
+    return content;
+  }
+  
+  // Escapes HTML characters in a template literal string, to prevent XSS.
+  // See https://www.owasp.org/index.php/XSS_%28Cross_Site_Scripting%29_Prevention_Cheat_Sheet#RULE_.231_-_HTML_Escape_Before_Inserting_Untrusted_Data_into_HTML_Element_Content
+  function sanitizeHTML(strings) {
+    const entities = {'&': '&amp;', '<': '&lt;', '>': '&gt;', '"': '&quot;', "'": '&#39;'};
+    let result = strings[0];
+    for (let i = 1; i < arguments.length; i++) {
+      result += String(arguments[i]).replace(/[&<>'"]/g, (char) => {
+        return entities[char];
+      });
+      result += strings[i];
+    }
+    return result;
+  }
+    
+  //https://developers.google.com/maps/solutions/store-locator/simple-store-locator
+  // http://www.ark-l-m.fi/projects/current-projects/
+})(google);
